02213bcb875ce5f5b7d6838477a44a2f284082b6
MRubaish Ali <mrubaish16@gmail.com>
2025-02-01 15:19:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var email = prompt("Add Email")
+
+if( email === ""){
+    alert()
+}
+var Passward = prompt("Passward")
+
+if( Passward === ""){
+    alert("You Are Successfully Login")
+}
+
+var email = [""]
+console.log("Email")
+
+var Passward = [""]
+console.log("Passward")
+alert("you are successful")
+var a = prompt("Enter your email")
+var b = prompt("Enter your password")
+var laptop = []
+laptop.push(a)
+laptop.push(b)
+console.log(laptop)
